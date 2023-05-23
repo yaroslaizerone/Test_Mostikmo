500dfa87d5bca4b1bcf9c7c4eb9952c16d771ecf
Yaroslav Kolpackov <kolpackov.yarosl@yandex.ru>
2023-05-23 19:30:34 +0700

    operation_recycle_view

---
+++ app/src/main/java/com/example/test/AddNewMoneyCardActivity.java
@@ -0,0 +1,83 @@
+package com.example.test;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddNewMoneyCardActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+    ImageView btBack;
+    EditText namecard;
+    TextView SaveText;
+    Spinner typeV, typeCard, typeBank;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_money_card);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+
+        btBack = findViewById(R.id.BackActiv);
+        namecard = findViewById(R.id.Inputnamecard);
+        SaveText = findViewById(R.id.savecl);
+        typeBank = findViewById(R.id.spinBank);
+        typeV = findViewById(R.id.spinValut);
+        typeCard = findViewById(R.id.typespin);
+
+        btBack.setOnClickListener(v-> Backctivity());
+        SaveText.setOnClickListener(v-> SaveLocation());
+    }
+    void SaveLocation(){
+        //TODO Сделать добавление нового счёта, сделать документ с логикой для хранения данных для каждого пользователя
+        String nameTitle = namecard.getText().toString();
+        Map<String, Object> userMoneySave = new HashMap<>();
+        userMoneySave.put("email", user.getEmail());
+        userMoneySave.put("namemoney", nameTitle);
+        userMoneySave.put("typecard", typeCard.getSelectedItem().toString());
+        userMoneySave.put("typevalut", typeV.getSelectedItem().toString());
+        userMoneySave.put("typebank", typeBank.getSelectedItem().toString());
+        userMoneySave.put("scoremoney", "0");
+
+        db.collection("userScore")
+                .add(userMoneySave)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(AddNewMoneyCardActivity.this, "Счёт был успешно добавленн.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+    void Backctivity(){
+        finish();
+    }
+}
